502a1fa3cf18ba8b5bdc6b4d663ae2a33e8337d4
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-01-30 10:01:27 +0545

    I tried to make the signup page data from Method go from there to welcome  but its not working I will fix it after classes. I also added new imports there for session which is used to take the data from there to in welcome. Later Chandani will add the profile page data as well and Ill work with it accordingly.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session,request,url_for, redirect
 
 app = Flask(__name__)
 
@@ -9,15 +9,36 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods = ['GET','POST'])
 def signup():
+    # this takeas the data from the user from the form
+    if request.method == "POST":
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        nickname = request.form.get('nickname')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        experience = request.form.get('experience')
+        cook_type = request.form.get('cook_type')
+
+
+  
+        return redirect(url_for("welcome"))
+
+
     return render_template('signup.html')
-
-
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html')
+    # this takes the data from the user or thier info to later print it out in the respective pages
+    name = session.get('name')
+    nickname = session.get('nickname')
+    email = session.get('email')
+    phone = session.get('phone')
+    experience = session.get('experience')
+    cook_type = session.get('cook_type')
+
+    return render_template('welcome.html', name=name,email=email,nickname=nickname,experience=experience,cook_type=cook_type)
 
 
 if __name__ == '__main__':
